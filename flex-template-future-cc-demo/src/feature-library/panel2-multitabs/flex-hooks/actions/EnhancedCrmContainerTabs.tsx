b6c914c13fbaf856093583d508bb332502c969db
Jason Learmouth <jason@learmouth.ca>
2023-10-23 15:58:35 +1100

    Add action hook for loading Enhanced CRM Container tabs

---
+++ flex-template-future-cc-demo/src/feature-library/panel2-multitabs/flex-hooks/actions/EnhancedCrmContainerTabs.tsx
@@ -0,0 +1,40 @@
+import * as Flex from '@twilio/flex-ui';
+
+import { FlexActionEvent } from '../../../../types/feature-loader';
+import { createEnhancedCrmContainerTabs } from '../../config';
+import SegmentView from '../../custom-components/Segment/SegmentView';
+import ConversationCardsCRM from '../../custom-components/ConversationCards/ConversationCardsCRM';
+import FauxCustomerProfile from '../../custom-components/FauxCustomerProfile/FauxCustomerProfileView';
+
+
+// If Enhanced CRM Container Tabs are enabled, register the individual components as Tabs
+export const actionEvent = FlexActionEvent.before;
+export const actionName = 'LoadCRMContainerTabs';
+export const actionHook = function addToEnhancedCRM(flex: typeof Flex, manager: Flex.Manager) {
+  flex.Actions.addListener(`${actionEvent}${actionName}`, async (payload) => {
+    // Only render when we have a task and createEnhancedCrmContainerTabs = true
+    if (!createEnhancedCrmContainerTabs() && !payload.task) {
+      return;
+    }
+
+    // Append with our component definition
+    payload.components = [
+      ...payload.components,
+      {
+        title: 'Segment',
+        order: 0, // optionally define preferred tab order, defaults to 999 if not present
+        component: <SegmentView key="segment-tab" />,
+      },
+      {
+        title: 'Cards',
+        order: 1, // optionally define preferred tab order, defaults to 999 if not present
+        component: <ConversationCardsCRM key="conversation-cards-tab" />,
+      },
+      {
+        title: 'Profile',
+        order: 2, // optionally define preferred tab order, defaults to 999 if not present
+        component: <FauxCustomerProfile key="faux-profile-tab" />,
+      },
+    ];
+  });
+};
