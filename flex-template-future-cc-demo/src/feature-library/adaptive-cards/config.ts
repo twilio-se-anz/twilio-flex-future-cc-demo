0aba053ac91c770b12e6818036c81553afd5d647
Jason Learmouth <jason@learmouth.ca>
2023-11-07 12:46:39 +1100

    Add config properties for tabs display

--- flex-template-future-cc-demo/src/feature-library/adaptive-cards/config.ts
+++ flex-template-future-cc-demo/src/feature-library/adaptive-cards/config.ts
@@ -1,8 +1,20 @@
 import { getFeatureFlags } from '../../utils/configuration';
 import AdaptiveCardsConfig from './types/ServiceConfiguration';
 
-const { enabled = false } = (getFeatureFlags()?.features?.adaptive_cards as AdaptiveCardsConfig) || {};
+const {
+  enabled = false,
+  show_cards_tab = false,
+  tab_order = 1,
+} = (getFeatureFlags()?.features?.adaptive_cards as AdaptiveCardsConfig) || {};
 
 export const isFeatureEnabled = () => {
   return enabled;
+};
+
+export const showCardsTab = () => {
+  return enabled && show_cards_tab;
+};
+
+export const tabOrder = () => {
+  return tab_order;
 };
