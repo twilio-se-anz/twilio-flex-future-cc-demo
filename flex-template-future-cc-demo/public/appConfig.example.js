2836e1767bcfbeb1684acb7ba5412d7e7c2a3511
Christopher Connolly <425070+chaosloth@users.noreply.github.com>
2023-11-07 21:13:52 +1100

    Add port numbers for custom domain

--- flex-template-future-cc-demo/public/appConfig.example.js
+++ flex-template-future-cc-demo/public/appConfig.example.js
@@ -8,8 +8,9 @@ var appConfig = {
   showSupervisorDesktopView: true,
   custom_data: {
     serverless_functions_protocol: 'http',
-    serverless_functions_port: '3001',
+    serverless_functions_port: '',
     serverless_functions_domain_future_cc_demo: 'localhost',
+    serverless_functions_port_future_cc_demo: '3001',
     common: {},
     features: {},
   },
